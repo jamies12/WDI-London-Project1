8e2d0912ad1880735d0878066af7f222b7cc6b9f
Jamie Simpson <jamie.shaun.simpson12@gmail.com>
2016-10-07 15:46:47 +0100

    Changed start placement of player/obstacle objects

--- js/app.js
+++ js/app.js
@@ -3,18 +3,29 @@ $(function() {
   playSpace = $('.gameSpace');
   character = $('.player');
   obstacle = $('.obstacle');
+  gameOver = $('.status');
+  clone = $(obstacle).clone(true);
 
   obstaclePosition = $(obstacle).position();
   characterPosition = $(character).position();
   console.log(characterPosition);
   console.log(obstaclePosition);
-
+  // console.log(clone);
 
   $(obstacle).click(function() {
     console.log('obstacle clicked');
-    (obstacle).animate({left: 0},3000);
+    (obstacle).animate({left: -50},2000, 'linear');
+    $collision();
+    // obstacle.append(clone);
+    // obstacle.fadeIn(20, 'linear');
   });
-  
 
+  $collision = (function () {
+    if(obstaclePosition <= characterPosition) {
+      $(gameOver).text = "COLLISION!";
+    }
+  });
+
+  $collision();
 
 });
