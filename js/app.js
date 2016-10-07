88ad7e70939c70a4afb6bd18dfdea9c413d292c8
Jamie Simpson <jamie.shaun.simpson12@gmail.com>
2016-10-07 16:40:16 +0100

    Added collision detection, however is slow to react to collision

--- js/app.js
+++ js/app.js
@@ -14,18 +14,40 @@ $(function() {
 
   $(obstacle).click(function() {
     console.log('obstacle clicked');
-    (obstacle).animate({left: -50},2000, 'linear');
-    $collision();
-    // obstacle.append(clone);
-    // obstacle.fadeIn(20, 'linear');
+    (obstacle).animate({left: -50},2000, 'linear', checkCollisions);
+
   });
 
-  $collision = (function () {
-    if(obstaclePosition <= characterPosition) {
-      $(gameOver).text = "COLLISION!";
-    }
-  });
+  function findPosition(obstacle) {
+    var $obstacle = $(obstacle);
+    var pos = $obstacle.position();
+    var width = $obstacle.width();
+    var height = $obstacle.height();
+    return [[pos.left, pos.left + width], [pos.top, pos.top + height]];
+  }
 
-  $collision();
+  function comparePositions(p1, p2) {
+    var x1 = p1[0] < p2[0] ? p1 : p2;
+    var x2 = p1[0] < p2[0] ? p2 : p1;
+    return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
+  }
+
+  function checkCollisions(){
+    var playerChar = $('.obstacle')[0];
+    var pos = findPosition(obstacle);
+
+    var pos2 = findPosition(this);
+    var horizontalMatch = comparePositions(pos[0], pos2[0]);
+    var verticalMatch = comparePositions(pos[1], pos2[1]);
+
+    var match = horizontalMatch && verticalMatch;
+    if (match) {console.log('COLLISION!');}
+  }
+
+
+
+
+
+
 
 });
